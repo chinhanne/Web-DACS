5a780174d3d1e144fb7dacecec49df9df1c34f31
chinhanne <cnhan111111@gmail.com>
2024-03-17 13:08:51 +0700

    init project

---
+++ demo/Controllers/PagesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+
+        public IActionResult ChackOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ demo/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult Shop_Detail()
+        {
+            return View();
+        }
+    }
+}
